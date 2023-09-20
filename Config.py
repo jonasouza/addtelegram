bcf999763cbb14ef7422c90ca19cbd7d0e617c20
jonasouza <124225309+jonasouza@users.noreply.github.com>
2023-09-19 22:24:03 -0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Get it from my.telegram.org
-API_ID = getenv("API_ID")
-API_HASH = getenv("API_HASH")
+API_ID = getenv("26480650")
+API_HASH = getenv("31cf7e6e42a77b0c6712f9c71e9dcfff")
 
 # Get it from @Botfather in Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("6191071968:AAEJkSvbMqLFFdORJpfmMsjr4pdcrG2f8A8")
